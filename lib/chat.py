a2902bfb752d4ba44431da0f8fbd62ff82e0be84
周恩頎 <114843168+sinchichou@users.noreply.github.com>
2024-11-29 10:54:46 +0800

    update chat.py

--- lib/chat.py
+++ lib/chat.py
@@ -34,14 +34,19 @@ class AIChatLibrary:
         with open('example.txt', 'w', encoding='utf-8') as file:
             file.write(reply + "\n\n")
 
-    def google_search(self, query):
-        url = "https://www.googleapis.com/customsearch/v1"
-        params = {
-            'key': self.google_api,  # 替換為你的API金鑰
-            'cx': self.cse_id,     # 替換為你的自定義搜尋引擎ID
-            'q': query
-        }
-        response = requests.get(url, params=params)
-        results = response.json().get('items', [])
-        return [item['link'] for item in results]
-       
+    # def google_search(self, query):
+    #     url = "https://www.googleapis.com/customsearch/v1"
+    #     params = {
+    #         'key': self.google_api,  # 替換為你的API金鑰
+    #         'cx': self.cse_id,     # 替換為你的自定義搜尋引擎ID
+    #         'q': query
+    #     }
+    #     response = requests.get(url, params=params)
+    #     results = response.json().get('items', [])
+    #     return [item['link'] for item in results]
+    def search(query):
+        url = "https://www.google.com/search?"
+        q_key = query
+        default_key = "&sourceid=chrome&ie=UTF-8"
+        response = requests.get(url + 'q=' + q_key + default_key)
+        
